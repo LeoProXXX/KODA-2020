717807d801c057a43bd9d72f9de175344d582dac
twlodarczyk <49198603+tomwlod@users.noreply.github.com>
2020-12-20 14:31:49 +0100

    add main file

---
+++ data/main.py
@@ -0,0 +1,5 @@
+# scenariusz weruyikacji kodu Golomba
+# - Przetestować algorytm na sztucznie wygenerowanych ciągach danych o rozkładzie równomiernym, normalnym, geometrycznym oraz obrazach testowych kodowanych różnicowo.
+# - Wyznaczyć histogram i entropię danych wejściowych.
+# - Porównać entropię ze średnią długością bitową kodu wyjściowego.
+# - Ocenić efektywność algorytmu do kodowania obrazów naturalnych.
